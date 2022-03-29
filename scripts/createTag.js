eae6a095a347b5fdd6849395cc37be251ef2a5f5
DVvor <85024600+DVvor@users.noreply.github.com>
2022-03-29 21:42:11 +0200

    close btntag

--- scripts/createTag.js
+++ scripts/createTag.js
@@ -1,22 +1,49 @@
+const tags = document.querySelectorAll('.tag');
+const tagSection = document.querySelector('.tag-section');
 const tag = document.querySelector('.tag');
-const thumbnailsSection = document.querySelector('.thumbnails-section');
+
+function tagSelected(event) {
+
+  if (!tagSection.textContent.includes(event.target.textContent)) {
+    event.target.style.backgroundColor = 'rgb(6, 62, 147, 0.3)';
+
+    const btnTag = document.createElement('div');
+    btnTag.classList = 'btn-tag';
+  
+    const tagName = document.createElement('p');
+    tagName.classList = 'tag-name';
+    tagName.textContent = event.target.textContent;
+  
+    const closeBtnTag = document.createElement('i');
+    closeBtnTag.classList = 'far fa-times-circle';
+  
+    
+    tagSection.appendChild(btnTag);
+    btnTag.appendChild(tagName);
+    btnTag.appendChild(closeBtnTag);
+    console.log(closeBtnTag)
+
+    closeBtnTag.addEventListener('click', closeTag)
+
+  function closeTag() {
+    tagSection.removeChild(btnTag);
+    event.target.style.backgroundColor = 'transparent';
 
 
-console.log(tag)
+  }
 
+  }
 
-
-function tagSelected() {
-  alert('test')
-
-  tag.style.backgroundColor = 'rgb(6, 62, 147, 0.3)';
-
-  const thumbnails = document.createElement('div');
-  thumbnails.classList = 'thumbnails';
-  thumbnails.textContent = 'Coco';
-  thumbnailsSection.appendChild(thumbnails);
+  
 
 
 }
+const createTag = tags.forEach((tag) => tag.addEventListener('click',tagSelected))
 
-tag.addEventListener('click',tagSelected)
+// const btnTagClose = document.querySelector('.fa-times-circle ');
+
+
+
+// barre de recherche filtre et propose les ingrédients en fonction de la recherche 
+// affichage des recettes en fonction des tags sélectionnés
+
